7718b6291a090a17627520bad8093a71bfb85655
Posad <posad.stanislavsky@gmail.com>
2025-02-27 19:32:26 +0300

    added new dict, and renaimed old

--- lexicon/lexicon_ru.py
+++ lexicon/lexicon_ru.py
@@ -1,1 +1,6 @@
-LEXICON_RU_COMMANDS = {'/start': 'Бот запущен...'}
+LEXICON_RU_MENU = {'/start': 'Это инлайн-кнопка с параметром "url"'}
+
+
+LEXICON_RU_INLINEBUTTONS = {'Курс "Телеграм-боты на Python и AIOgram"': 'https://stepik.org/120924',
+                            'Документация Telegram Bot API': 'https://core.telegram.org/bots/api',
+                            }
